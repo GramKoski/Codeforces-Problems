8929d17b712652aa82552ae93ec341659fb1f9c7
GramKoski <gkoski@ucsd.edu>
2025-03-14 23:32:56 -0700

    Problems 3/14

--- 1967A.cpp
+++ 1967A.cpp
@@ -11,24 +11,28 @@ int main() {
 	while (t--) {
 		int n, k; cin >> n >> k;
 		vector<int> freq(n, 0);
-		int a;
 		for (int i = 0; i < n; ++i) {
 			cin >> freq[i];
 		}
 		sort(freq.begin(), freq.end());
-		int i = 1; int res = 0;
-		int curr = freq[0];
-		res += curr*n;
-		while (i < n) {
-			if (freq[i] > curr) {
-				res += min(k, (i)*(freq[i]-curr));
-				k -= (i)*(freq[i]-curr);
-				res += (n-i);
-				curr = freq[i];
+		int res = 0;
+		//Key observation: It only matters what the minimum possible value is because that determines the number of "pure" permutations there will be in the list.
+		//The number of minimum frequency elements determines the number of extra permutations that can be appended to the end. If we increased a minumum frequence element by
+		// 1 we only increase the number of total permutations by 1 because we append at the end and rearrange if necessary. It only increases by one assuming that the permutation
+		// was in the correct arrangement before
+		// Note that this is the part where we can append one permutation for each element that is in excess
+		int w = freq[0];
+		for (int i = 1; i < n; ++i) {
+			if (k - ((freq[i] - freq[i-1]) * i) >= 0) {
+				k -= (freq[i]-freq[i-1]) * i;
+				w = freq[i];
+			} else {
+				w = w + k/i;
+				res -= i;
+				break;
 			}
-			i += 1;
-			if (k < 0) break;
 		}
+		res += n*w;
 		cout << res + max(0, k) << "\n";
 	}
 }
