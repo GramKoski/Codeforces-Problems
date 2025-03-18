1038ba2ffcd7cf2d893edf04b87cc7aebb62f8c0
GramKoski <gkoski@ucsd.edu>
2025-03-17 23:42:55 -0700

    Problems 3/17

---
+++ 1926E.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n, k; cin >> n >> k;
+		//Nothing will happen at odd numbered steps because odd * odd = odd
+		//Note that 2 brings even numbers that are divisible by 2 but there is no way for these numbers to be divisible by 4
+		//Note that 4 brings even nubmers that are divisible by 4, 2 has already been covereed and there is no way for these numbers to be divisible by 6 or more
+		//Note 6 doesn't bring anything new because a number because x*6 = y --> x*3*2 = y so it was covered in round 2
+		//In general if the round is not a power of 2 it can be broken down into an odd number and a lower power of 2
+		//What can be done checking if the number is odd than checking its lowest power of 2 (lowest set bit
+		//Or we can do it recursively is better
--- 1933E.cpp
+++ 1933E.cpp
@@ -6,17 +6,15 @@ using namespace std;
 int main() {
 	int t; cin >> t;
 	while (t--) {
-		//Find the greatest r st presum is max <= k. Also check r + 1 to see if it's better
+		//Find the greatest r st a is max <= k. Also check r + 1 to see if it's better
 		// Anything larger than r + 1 definetely won't be better
 		
 		int n; cin >> n;
 		vector<int> a(n);
-		vector<int> presum(n);
 		for (int i = 0; i < n; ++i) {
 			cin >> a[i];
 		}
-		presum[0] = a[0];
-		for (int i = 1; i < n; ++i) presum[i] += presum[i-1];
+		for (int i = 1; i < n; ++i) a[i] += a[i-1];
 		int q; cin >> q; vector<int> res;
 		int l, u;
 		while (q--) {
@@ -26,17 +24,19 @@ int main() {
 			int li = l-1-1; int ri = n-1;
 			while (li < ri -1) {
 				m = (li+ri) / 2;
-				if (presum[m]- ((l > 1) ? presum[l-1] : 0) >= u) {
+				//cout << "ri " << ri << " " << a[m]- ((l > 1) ? a[l-2] : 0) << "\n";
+				if (a[m]- ((l > 1) ? a[l-2] : 0) >= u) {
+					//cout << "m " << m << " " << a[m]- ((l > 1) ? a[l-2] : 0) << "\n";
 					ri = m;
 				} else {
 					li = m;
 				}
 			}
-			int sum = presum[m]-((l > 1) ? presum[l-1] : 0);
-			cout << "ri, sum " << ri << " " << sum << "\n";
-			if (ri > 0 && sum > u + 1) {
-				int sum_left = presum[ri-1] - ((l-1 > 0) ? presum[l-1] : 0);
-				if (u*(u-1)/2 - (sum - u -1)*(sum - u - 2)/2 > sum_left) {
+			int sum = a[ri]-((l > 1) ? a[l-2] : 0);
+			//cout << "ri, sum " << ri << " " << sum << "\n";
+			if (ri > 0  && ri > l-1 && sum > u + 1) {
+				int sum_left = a[ri-1] - ((l-1 > 0) ? a[l-2] : 0);
+				if (u*(u+1)/2 - (sum - u -1)*(sum - u)/2 > (u*(u+1)/2 - (u-sum_left)*(u-sum_left+1)/2)) {
 					res.push_back(ri+1);
 				} else {
 					res.push_back(ri);
