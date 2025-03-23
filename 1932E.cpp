6d2cbfa16c0f6f4a5fa26e601ff91cefe17e7cac
GramKoski <gkoski@ucsd.edu>
2025-03-22 23:19:13 -0700

    Problem 3/22

--- 1932E.cpp
+++ 1932E.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+#include <cstring>
+#include <algorithm>
+
 
 #define ll long long
 
@@ -12,13 +15,30 @@ int main() {
 	while (t--) {
 		ll n; cin >> n;
 		string s; cin >> s;
-		ll val = 0; ll res = 0;
-		for (ll i = 0; i < n; ++i) {
-			val *= 10;
-			val += (s[i]-'0');
-			res += (val-val/10)*(n-i);
-			cout << val << " " << res << "\n";
+		vector<int> pre(n);
+		pre[0] = (int)(s[0] - '0');
+		for (int i = 1; i < n; ++i) {
+			pre[i] = (int)(s[i] - '0') + pre[i-1];
 		}
+		//Use clever carrying algo to calculate the actual sum from the prefix sum
+		reverse(pre.begin(), pre.end());
+		string res;
+		int c = 0;
+		for (int i = 0; i < n; ++i) {
+			c += pre[i];
+			res += (char)(c%10 + '0');
+			c /= 10;
+		}
+		while (c != 0) {
+			res += (char)(c%10+'0');
+			c /= 10;
+		}
+		//Remove leading zeros
+		int l = res.length();
+		for (int i = l - 1; res[i] == '0' && i >= 0; --i) {
+			res.pop_back();
+		}
+		reverse(res.begin(), res.end());
 		cout << res << "\n";
 	}
 }
