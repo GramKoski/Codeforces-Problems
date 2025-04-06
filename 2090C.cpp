6a520658e38aa9b1ca12493533988ebc8daabaaf
GramKoski <gkoski@ucsd.edu>
2025-04-05 23:39:54 -0700

    Problems 4/5

--- 2090C.cpp
+++ 2090C.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <set>
 
 using namespace std;
 
@@ -12,24 +13,26 @@ int main() {
 		for (int i = 0; i < n; ++i) {
 			cin >> t[i];
 		}
+		//BFS to find distance to n tables
+		//n by n list to see if table is occupied
+		//two queues, one for tables (bottom left corner of table) and another for table cell
 		queue<pair<int, int>> q1;
 		queue<pair<int, int>> q2;
-		q1.insert({1, 1});
-		q2.insert({1, 1});
-		set<pair<int, int>> v;
-		vector<pair<int, int>> m(n);
-		for (int i = 0; i < n; ++i) {
-			if (t[i] == false) {
-				pair<int, int> curr = q.front();
-				q1.pop();
-				while (v.find(curr) != v.end()) {
-					curr = q.front();
-					q1.pop();
+		queue<pair<int, int>> q;
+		q1.push({1, 1});
+		q2.push({1, 1});
+		set<int> s;
+		pair<int, int> curr;
+		while (!q.empty()) {
+			int l = q.size();
+			for (int i = 0; i < l; ++i) {
+				curr = q.front();
+				if (s.find(curr) != s.end()) {
+					continue;
 				}
-				v.insert(curr);
+				s.emplace(curr);
 				if (curr.first % 3 == 1 && curr.second % 3 == 1) {
-					q1.insert({curr.first, curr.second + 1});
-				} else if (curr.first % 3 == 2 && curr.second % 3 == 1) {
-					q1.insert({curr.first-1, curr.second + 1});
-				} else if (curr.first % 2 == 1 && curr.second % 3 == 2) {
-					curr
+					q2.push_back(curr);
+				}
+				if (1 <= curr.first % 3 && 2 >= curr.first % 3 && 1 <= curr.second % 3 && 
+
