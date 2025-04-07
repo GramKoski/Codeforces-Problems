1b3ac051a32a14588e34b825dbf93f5b422ad57f
GramKoski <gkoski@ucsd.edu>
2025-04-06 23:53:12 -0700

    Problems 4/6

--- 2090C.cpp
+++ 2090C.cpp
@@ -9,7 +9,7 @@ int main() {
 	int t; cin >> t;
 	while (t--) {
 		int n; cin >> n;
-		vector<bool> t(n);
+		vector<int> t(n);
 		for (int i = 0; i < n; ++i) {
 			cin >> t[i];
 		}
@@ -19,20 +19,56 @@ int main() {
 		queue<pair<int, int>> q1;
 		queue<pair<int, int>> q2;
 		queue<pair<int, int>> q;
-		q1.push({1, 1});
-		q2.push({1, 1});
-		set<int> s;
+		q.push({0, 0});
+		set<pair<int, int>> s;
 		pair<int, int> curr;
-		while (!q.empty()) {
+		while (!q.empty() && q2.size() < n) {
 			int l = q.size();
 			for (int i = 0; i < l; ++i) {
-				curr = q.front();
-				if (s.find(curr) != s.end()) {
-					continue;
-				}
+				curr = q.front(); q.pop();
+				//cout << curr.first << " " << curr.second << "\n";
+				if (s.find(curr) != s.end()) continue; 
 				s.emplace(curr);
 				if (curr.first % 3 == 1 && curr.second % 3 == 1) {
-					q2.push_back(curr);
+					q2.push(curr);
 				}
-				if (1 <= curr.first % 3 && 2 >= curr.first % 3 && 1 <= curr.second % 3 && 
+				if (1 <= curr.first % 3 && 1 <= curr.second % 3) {
+					q1.push(curr);
+				} else {
+					for (int x = -1; x < 2; ++x) {
+						if (x == 0 && curr.second > 0) {
+							pair<int, int> below = {curr.first, curr.second -1};
+							if (s.find(below) == s.end()) q.push(below);
+						}
+						pair<int, int> n = {curr.first + x, curr.second + ((x == 0) ? 1 : 0)};
+						if (s.find(n) == s.end() && curr.first +x >= 0) {
+							q.push(n);
+						} 
 
+					}
+				}
+			}
+		}
+		set<pair<int, int>> v;
+		for (int i = 0; i < n; ++i) {
+			if (t[i] == 1) {
+				while (v.find(q1.front()) != v.end()) {
+					q1.pop();
+				}
+				cout << q1.front().first << " " << q1.front().second << "\n";
+				if (q1.front() == q2.front()) {
+					q2.pop();
+				}
+				v.emplace(q1.front());
+				q1.pop();
+			} else {
+				cout << q2.front().first << " " << q2.front().second << "\n";
+				v.emplace(q2.front());
+				q2.pop();
+			}
+		}
+		//cout << "\n";
+	}
+}
+
+			
